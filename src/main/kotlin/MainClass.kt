54b791f02deb34beafe059622d8bc1bff06b6be4
Rohan Taneja <rohan.bvcoe@gmail.com>
2017-11-21 16:32:05 +0530

    added transactions menu

--- src/main/kotlin/MainClass.kt
+++ src/main/kotlin/MainClass.kt
@@ -4,6 +4,11 @@ package main.kotlin
 Created by rohan on 20/11/17  
 */
 
-fun main(args: Array<String>){
+fun main(args: Array<String>) {
+    println("Welcome to my bank")
 
+    println("Choose operation:")
+    println("1. Withdraw")
+    println("2. Deposit")
+    println("3. Exit")
 }
