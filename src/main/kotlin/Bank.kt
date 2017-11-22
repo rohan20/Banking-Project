76e4661ced598b40e971391626b5aa63919f3cd5
Rohan Taneja <rohan.bvcoe@gmail.com>
2017-11-22 15:02:56 +0530

    added class bank

---
+++ src/main/kotlin/Bank.kt
@@ -0,0 +1,25 @@
+package main.kotlin
+
+/*
+Created by rohan on 22/11/17  
+*/
+
+class Bank() {
+
+    var currentAmount = 0.00
+
+    fun withdrawAmount(amount: Double): Double {
+        if (amount > currentAmount) {
+            println("Not enough amount in bank.")
+            return -1.0
+        } else {
+            currentAmount -= amount
+            return currentAmount
+        }
+    }
+
+    fun depositAmount(amount: Double): Double {
+        currentAmount += amount
+        return currentAmount
+    }
+}
