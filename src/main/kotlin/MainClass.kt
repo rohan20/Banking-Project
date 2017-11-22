398b226bcc1bb54e2670a5a95dd55b82ba0ef68c
Rohan Taneja <rohan.bvcoe@gmail.com>
2017-11-22 15:00:06 +0530

    if-else for withdraw/deposit

--- src/main/kotlin/MainClass.kt
+++ src/main/kotlin/MainClass.kt
@@ -5,8 +5,28 @@ Created by rohan on 20/11/17
 */
 
 fun main(args: Array<String>) {
-    println("Welcome to my bank")
 
+    val WITHDRAW = 1
+    val DEPOSIT = 2
+    val EXIT = 3
+
+    println("Welcome to my bank")
+    var userChoice: Int = 0
+
+    while (userChoice != EXIT) {
+        showChoices()
+        userChoice = readLine()!!.toInt()
+
+        if (userChoice == WITHDRAW) {
+
+        } else if (userChoice == DEPOSIT) {
+
+        }
+
+    }
+}
+
+fun showChoices() {
     println("Choose operation:")
     println("1. Withdraw")
     println("2. Deposit")
