af83c2bfbe3e91505f08c7553a10b27c1dbb02c5
Rohan Taneja <rohan.bvcoe@gmail.com>
2017-11-23 22:40:26 +0530

    ? operator for null safety

--- src/main/kotlin/Bank.kt
+++ src/main/kotlin/Bank.kt
@@ -6,24 +6,24 @@ Created by rohan on 22/11/17
 
 class Bank() {
 
-    var currentAmount = 0.00
+    var currentAmount = 2.0
 
-    fun withdrawAmount(amount: Double): Double {
-        if (amount > currentAmount) {
-            println("Not enough amount in bank.")
-            return -1.0
-        } else {
-            currentAmount -= amount
-            return currentAmount
-        }
-    }
-
-    fun depositAmount(amount: Double): Double {
-        currentAmount += amount
-        return currentAmount
-    }
-
-    fun getCurrentBalance(): Double {
-        return currentAmount
-    }
+//    fun withdrawAmount(amount: Double): Double {
+//        if (amount > currentAmount) {
+//            println("Not enough amount in bank.")
+//            return -1.0
+//        } else {
+//            currentAmount -= amount
+//            return currentAmount
+//        }
+//    }
+//
+//    fun depositAmount(amount: Double): Double {
+//        currentAmount += amount
+//        return currentAmount
+//    }
+//
+//    fun getCurrentBalance(): Double {
+//        return currentAmount
+//    }
 }
--- src/main/kotlin/MainClass.kt
+++ src/main/kotlin/MainClass.kt
@@ -13,26 +13,28 @@ var userChoice = 0
 fun main(args: Array<String>) {
 
 
-    println("Welcome to my bank")
-    val bank: Bank = Bank()
+//    println("Welcome to my bank")
+//    val bank: Bank = Bank()
 
-    while (userChoice != EXIT) {
-        performBankTask(bank);
-    }
+//    while (userChoice != EXIT) {
+//        performBankTask(bank);
+//    }
+
+    checkingNulls()
 }
 
 fun performBankTask(bank: Bank) {
-    showChoices()
-    displayCurrentBalance(bank)
-    userChoice = readLine()!!.toInt()
-
-    if (userChoice == WITHDRAW) {
-        bank.withdrawAmount(withdraw())
-    } else if (userChoice == DEPOSIT) {
-        bank.depositAmount(deposit())
-    }
-
-    displayCurrentBalance(bank)
+//    showChoices()
+//    displayCurrentBalance(bank)
+//    userChoice = readLine()!!.toInt()
+//
+//    if (userChoice == WITHDRAW) {
+//        bank.withdrawAmount(withdraw())
+//    } else if (userChoice == DEPOSIT) {
+//        bank.depositAmount(deposit())
+//    }
+//
+//    displayCurrentBalance(bank)
 }
 
 fun withdraw(): Double {
@@ -53,6 +55,14 @@ fun showChoices() {
 }
 
 fun displayCurrentBalance(bank: Bank) {
-    println("Current Balance: " + bank.getCurrentBalance())
+//    println("Current Balance: " + bank.getCurrentBalance())
     println()
+}
+
+fun checkingNulls(){
+    val listOfBanks = listOf(Bank(), Bank(), null, Bank())
+    listOfBanks.forEach { bank ->
+        val money = bank?.currentAmount
+        println(money)
+    }
 }
